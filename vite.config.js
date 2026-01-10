725f60114732342ac1b72a452583597b3d2e5192
Mohit-Gosai <mohitgosai.officle106@gmail.com>
2026-01-10 11:46:10 +0530

    Add deployment workflow and base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or @vitejs/plugin-react
+import react from '@vitejs/react-swc' // or @vitejs/plugin-react
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-notes/', // Add this line!
+  base: "/React-notes/", // ADD THIS LINE
 })
