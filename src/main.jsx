8ae110c0e8052db3fc44024bee7f277c1ec22369
mohit-gosai <mohitgoasi.officle106@gmail.com>
2026-01-12 10:26:44 +0530

    fixing the react router issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
-import Introduction from "./Pages/IntroductionPage";
-import HtMLJsPage from "./Pages/HtmlJsPage";
-import ComponentsPage from "./Pages/ComponentsPage";
-import PropsPage from "./Pages/PropsPage";
-import StatePage from "./Pages/StatePage";
-import HooksPage from "./Pages/HooksPage";
-import ReactRouterPage from "./Pages/ReactRouterPage";
-import DataFlowPage from "./Pages/core-concepts/DataFlowPage";
-import ComponentLifecyclePage from "./Pages/core-concepts/ComponentLifecyclePage";
-import StateVsPropsPage from "./Pages/core-concepts/StateVsPropsPage";
-import RenderingVirtualDOMPage from "./Pages/core-concepts/RenderingVirtualDOMPage";
-import ControlledComponentsPage from "./Pages/core-concepts/ControlledComponentsPage";
+import Introduction from "./Pages/IntroductionPage.jsx";
+import HtMLJsPage from "./Pages/HtmlJsPage.jsx";
+import ComponentsPage from "./Pages/ComponentsPage.jsx";
+import PropsPage from "./Pages/PropsPage.jsx";
+import StatePage from "./Pages/StatePage.jsx";
+import HooksPage from "./Pages/HooksPage.jsx";
+import ReactRouterPage from "./Pages/ReactRouterPage.jsx";
+import DataFlowPage from "./Pages/core-concepts/DataFlowPage.jsx";
+import ComponentLifecyclePage from "./Pages/core-concepts/ComponentLifecyclePage.jsx";
+import StateVsPropsPage from "./Pages/core-concepts/StateVsPropsPage.jsx";
+import RenderingVirtualDOMPage from "./Pages/core-concepts/RenderingVirtualDOMPage.jsx";
+import ControlledComponentsPage from "./Pages/core-concepts/ControlledComponentsPage.jsx";
 
 
 const router = createBrowserRouter([
